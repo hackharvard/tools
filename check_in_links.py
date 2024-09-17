3f04cd2bbd7567e9f2974c39125a667dcb63161b
ivanharvard <ivangutierrez@college.harvard.edu>
2024-09-16 23:14:02 -0400

    added name, hhid, email association to check in link

--- check_in_links.py
+++ check_in_links.py
@@ -1,4 +1,4 @@
-from helpers import get_uids, get_applicant_hhid, CONFIRMATIONS_COLLECTION
+from helpers import get_uids, get_applicant_hhid, get_accepted_applications
 
 """
 Associates a uid with a link to check-in
@@ -6,16 +6,23 @@ Associates a uid with a link to check-in
 
 def generate_checkin_links():
     """
-    Generate check-in-links for all application ids
+    Generate check-in-links for all accepted applicants
     """
-    uids = get_uids(CONFIRMATIONS_COLLECTION)
+    applications = get_accepted_applications()
     checkin_links = {}
-    for uid in uids:
-        hhid = get_applicant_hhid(uid)
-        checkin_links[uid] = (
-            f"https://admin.hackharvard.io/user/{hhid}"
-        )
-
+    for application_id, application_data in applications.items():
+        first_name = application_data["personal"]["firstName"]
+        last_name = application_data["personal"]["lastName"]
+        uid = application_id
+        hhid = application_data["meta"]["hhid"]
+        email = application_data["personal"]["email"]
+        checkin_links[uid] = {
+            "name": f"{first_name} {last_name}",
+            "hhid": hhid,
+            "email": email,
+            "url": f"https://admin.hackharvard.io/user/{hhid}"
+        }
+            
     return checkin_links
 
 if __name__ == "__main__":
